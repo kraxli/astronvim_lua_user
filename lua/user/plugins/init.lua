85943cc6bb53585cba0455008ae07908d45fe243
kraxli <kraxli@pm.me>
2022-09-25 13:58:16 +0200

    plugins

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -40,6 +40,7 @@ return {
 	-- -------------------------------------------------------
 	-- KRAXLI ADDINGS:
 	-- -------------------------------------------------------
+
 	{
 		"tpope/vim-fugitive",
 		cmd = "Git",
@@ -47,6 +48,7 @@ return {
 
 	-- Lsp:
 	{
+		"ray-x/lsp_signature.nvim",
 		event = "BufRead",
 		-- event = { "BufRead", "BufNew" },
 		config = function()
