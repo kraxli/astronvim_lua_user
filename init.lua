23801f9e030d7881e820cefafadc7287656cc4c1
kraxli <kraxli@pm.me>
2022-10-19 07:38:14 +0200

    add markdown/tex plugins

--- init.lua
+++ init.lua
@@ -3,6 +3,6 @@ IS_WINDOWS = (vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1)
 -- add a directory to runtimerpath:
 -- vim.opt.rtp:append("~/.config/astronvim/lua/user")
 
-require("user.local.settings")
+pcall(require, "local.settings")
 
 return {}
